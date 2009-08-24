c35fb0279c0d9bc79afdf24046ee1816c2985a75
Nicolas Mérouze <nicolas.merouze@gmail.com>
2009-08-24 14:51:38 +0200

    Fix to be totally agnostic.

--- rails/init.rb
+++ rails/init.rb
@@ -1,12 +1,15 @@
 File.join(File.dirname(__FILE__), "../lib/agnostic_presenters")
-AgnosticPresenters::Base.send :include, ActionView::Helpers if defined?(Rails)
 
-class << ActionController::Base
-  def add_template_helper_with_presenters(helper_module, *args, &block)
-    # Hijacking any helper added to controllers so that our presenters can acces 'em.
-    AgnosticPresenters::Base.instance_eval { include helper_module }
-    add_template_helper_without_presenters(helper_module, *args, &block)
-  end
+if defined?(Rails)
+  AgnosticPresenters::Base.send :include, ActionView::Helpers 
+
+  class << ActionController::Base
+    def add_template_helper_with_presenters(helper_module, *args, &block)
+      # Hijacking any helper added to controllers so that our presenters can acces 'em.
+      AgnosticPresenters::Base.instance_eval { include helper_module }
+      add_template_helper_without_presenters(helper_module, *args, &block)
+    end
   
-  alias_method_chain :add_template_helper, :presenters
+    alias_method_chain :add_template_helper, :presenters
+  end
 end
